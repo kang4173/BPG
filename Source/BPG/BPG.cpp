92554cc87ceb9387f6137e9bc4d9feffb38b60a1
Kanghojae <o8117o@naver.com>
2023-06-21 14:39:01 +0900

    혈압마라톤 시작

---
+++ Source/BPG/BPG.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BPG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BPG, "BPG" );
+ 
---
+++ Source/BPG/BPGGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BPGGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABPGGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABPGGameMode();
+};
+
+
+
